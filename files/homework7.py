56c51913802b9682713c43130b3549e4e28feb0b
eTcilopp <kirikeza@gmail.com>
2023-06-08 20:29:45 -0600

    Solved problem 36

--- files/homework7.py
+++ files/homework7.py
@@ -28,3 +28,25 @@ def get_critique(poem):
 
 assert get_critique('пара-ра-рам рам-пам-папам па-ра-па-да') == 'Парам пам-пам'
 assert get_critique('пара-ра-рам рам-пам-папам па-ра-па-да-да-па') == 'Пам парам'
+
+'''
+Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
+которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца. 
+Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, 
+которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). 
+Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например, у операции умножения.
+
+'''
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for row in range(1, num_rows +1):
+        print()
+        for col in range(1, num_columns + 1):
+            print(operation(row, col), end=' ')
+
+print_operation_table(lambda x, y: x * y)
+
+def my_sum(a, b):
+    return a**b
+
+print_operation_table(my_sum)

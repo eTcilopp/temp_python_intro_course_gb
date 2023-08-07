730b6710f415ac8396a12da3727bee0a6c2e9d7a
eTcilopp <kirikeza@gmail.com>
2023-08-07 13:28:44 -0600

    minor changes

---
+++ files/temp.py
@@ -0,0 +1,17 @@
+import base64
+
+def convert_image_to_string(image_path):
+    with open(image_path, "rb") as image_file:
+        image_bytes = image_file.read()
+        encoded_image = base64.b64encode(image_bytes).decode("utf-8")
+        image_string = f"data:image/png;base64,{encoded_image}"
+        return image_string
+
+# Usage example
+image_path = "C:\\temp\\Quanta-Services.png"
+result_path = "C:\\temp\\Output.txt"
+image_string = convert_image_to_string(image_path)
+with open(result_path, "w") as result_path:
+    result_path.write(image_string)
+    
+print('Done')
